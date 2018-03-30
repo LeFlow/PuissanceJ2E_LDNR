8d3ff56a57e8e9b587bc4cccae7008c0b8837bff
Jérémy DUFOUR <jeremydufour@MacBook-Pro-de-Jeremy.local>
2018-03-30 12:03:02 +0200

    Modification de la classe PlayerDAO

--- ProjetJEE/src/jee/DAO/pack/PlayerDAO.java
+++ ProjetJEE/src/jee/DAO/pack/PlayerDAO.java
@@ -1,5 +1,23 @@
 package jee.DAO.pack;
 
-public class PlayerDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
+public class PlayerDAO {
+	
+	private Connection conn;
+	
+	public PlayerDAO() throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:8889/puissancebase","ldnr","groupe3");
+		conn.createStatement().execute("CREATE TABLE IF NOT EXISTS JOUEURS(ID int primary key AUTO_INCREMENT NOT NULL,PSEUDO VARCHAR(100))");
+	}
+	
+	public void insererJoueur (String pseudo) throws Exception{
+		PreparedStatement ps = conn.prepareStatement("INSERT INTO JOUEURS (pseudo) VALUES(?)");
+		ps.setString(1,pseudo);
+		ps.executeUpdate();
+	}
+	
 }
