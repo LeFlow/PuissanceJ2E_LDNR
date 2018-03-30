f041e098b190b2e981b1ff3273b80198826780cf
LeFlow <florent.prieur@gmail.com>
2018-03-30 10:52:48 +0200

    Modif head.jsp, Servlets

--- ProjetJEE/src/jee/servlet/pack/indexServlet.java
+++ ProjetJEE/src/jee/servlet/pack/indexServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class indexServlet
  */
-@WebServlet("/")
+@WebServlet("/index.html")
 public class indexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
