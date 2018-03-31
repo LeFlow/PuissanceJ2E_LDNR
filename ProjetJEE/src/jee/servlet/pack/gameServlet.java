7dd71d67d08dfa592bf8eaed018bffed6bb5bd2a
Jérémy DUFOUR <jeremydufour@MacBook-Pro-de-Jeremy.local>
2018-03-31 08:30:58 +0200

    Rajout de la servlet pour la page game.jsp

---
+++ ProjetJEE/src/jee/servlet/pack/gameServlet.java
@@ -0,0 +1,42 @@
+package puissanceldnrproject;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial") 
+@WebServlet("/game")
+
+public class JeuServlet extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) 
+					throws ServletException, IOException {
+		
+		if(req.getMethod().equals("POST")) {
+			getServletContext().log("ajout dans la table partie");
+		try {
+				String chaineadresse = req.getParameter("adressecase");
+				String positionx = chaineadresse.substring(2,3);
+				String positiony = chaineadresse.substring(6,7);
+				
+				//new JoueurDAO().insererJoueur(
+				//	req.getParameter("pseudo"));
+				
+				new PartieDAO().insererPartie(
+					req.getParameter("num_partie"),
+					req.getParameter("joueur"),
+					positionx,
+					positiony);
+				
+			}catch (Exception ex) {getServletContext().log("Exc:"+ ex);}
+		}
+		
+		req.getRequestDispatcher("WEB-INF/views/game.jsp").include(req, res);
+		
+	}
+
+}
