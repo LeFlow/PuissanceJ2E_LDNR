08fa763987f84733fb6a79dddccf6b74e2935d2c
Jérémy DUFOUR <jeremydufour@MacBook-Pro-de-Jeremy.local>
2018-04-01 17:17:26 +0200

    Rajout des solutions dans la gameServlet

--- ProjetJEE/src/jee/servlet/pack/gameServlet.java
+++ ProjetJEE/src/jee/servlet/pack/gameServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import puissanceldnrproject.PartieBean;
+import puissanceldnrproject.PartieDAO;
+
 @SuppressWarnings("serial") 
 @WebServlet("/game")
 
@@ -24,6 +27,7 @@ public class gameServlet extends HttpServlet{
 		
 		//on récupère sur la page la value ou le name est "joueur"
 		String idjoueur = req.getParameter("joueur");
+		String idjoueur2 = req.getParameter("joueur2");
 		
 		//on récupère sur la page la value ou le nam est "adressecase"
 		String adressecase = req.getParameter("adressecase");
@@ -31,7 +35,7 @@ public class gameServlet extends HttpServlet{
 		//on crée un tableau ou l'on stocke ces valeurs.
 		String valeurcocheetab[] = {" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "};
 
-		if(req.getMethod().equals("POST")) {
+		if(req.getMethod().equals("GET")) {
 			getServletContext().log("ajout dans la table partie");
 			try {
 					new PartieDAO().insererPartie(
@@ -51,6 +55,7 @@ public class gameServlet extends HttpServlet{
 		
 		//on créer une ArrayList des valeurrecuperer pour les positions pour "un joueur et une partie donnée"
 		ArrayList<String> valeurrecuperer = new ArrayList<>();
+		ArrayList<String> valeurrecuperer2 = new ArrayList<>();
 		
 		//on remplit dans des objets partie bean avec le même numéro de partie et le même idjoueur
 		//la liste valeurrecuperer
@@ -60,6 +65,84 @@ public class gameServlet extends HttpServlet{
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		try {
+			for (PartieBean partieitem : (new PartieDAO()).coordonneesBouton(numeropartie, idjoueur2))
+				valeurrecuperer2.add(partieitem.getAdressecase());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		if ( 
+		//Solution 1 : case 0,1,2,3 cochee
+		( valeurrecuperer.contains("0") && valeurrecuperer.contains("1") && valeurrecuperer.contains("2") && valeurrecuperer.contains("3") ) ||
+		//Solution 2 : case 1,2,3,4 cochee
+	    ( valeurrecuperer.contains("1") && valeurrecuperer.contains("2") && valeurrecuperer.contains("3") && valeurrecuperer.contains("4") ) ||
+	    //Solution 3 : case 5,6,7,8 cochee
+	    ( valeurrecuperer.contains("5") && valeurrecuperer.contains("6") && valeurrecuperer.contains("7") && valeurrecuperer.contains("8") ) ||	
+	    //Solution 4: case 6,7,8,9 cochee
+	    ( valeurrecuperer.contains("6") && valeurrecuperer.contains("7") && valeurrecuperer.contains("8") && valeurrecuperer.contains("9") ) ||
+	    //Solution 5 : case 10,11,12,13 cochee	
+	    ( valeurrecuperer.contains("10") && valeurrecuperer.contains("11") && valeurrecuperer.contains("12") && valeurrecuperer.contains("13") ) ||	
+	    //Solution 6 : case 11,12,13,14 cochee
+	    ( valeurrecuperer.contains("11") && valeurrecuperer.contains("12") && valeurrecuperer.contains("13") && valeurrecuperer.contains("14") ) ||	
+	    //Solution 7 : case 15,16,17,18 cochee
+	    ( valeurrecuperer.contains("15") && valeurrecuperer.contains("16") && valeurrecuperer.contains("17") && valeurrecuperer.contains("18") ) ||	
+	    //Solution 8 : case 16,17,18,19 cochee
+	    ( valeurrecuperer.contains("16") && valeurrecuperer.contains("17") && valeurrecuperer.contains("18") && valeurrecuperer.contains("19") ) ||	
+	    //Solution 9 : case 20, 21, 22, 23
+	    ( valeurrecuperer.contains("20") && valeurrecuperer.contains("21") && valeurrecuperer.contains("22") && valeurrecuperer.contains("23") ) ||	
+	    //Solution 10 : case 21 , 22, 23 ,24 
+	    ( valeurrecuperer.contains("21") && valeurrecuperer.contains("22") && valeurrecuperer.contains("23") && valeurrecuperer.contains("24") ) ||
+	    //Solution 11 : case 0, 5, 10 , 15
+	    ( valeurrecuperer.contains("0") && valeurrecuperer.contains("5") && valeurrecuperer.contains("10") && valeurrecuperer.contains("15") ) ||
+		//Solution 12 : case 5, 10,15 , 20
+	    ( valeurrecuperer.contains("5") && valeurrecuperer.contains("10") && valeurrecuperer.contains("15") && valeurrecuperer.contains("20") ) ||
+	    //Solution 13 : case 1,6,11,21
+	    ( valeurrecuperer.contains("1") && valeurrecuperer.contains("6") && valeurrecuperer.contains("11") && valeurrecuperer.contains("16") ) ||
+	    //Solution 14 : case 6,11,16,21
+	    ( valeurrecuperer.contains("6") && valeurrecuperer.contains("11") && valeurrecuperer.contains("16") && valeurrecuperer.contains("21") ) ||
+	    //Solution 15 : case 2,7,12,17
+	    ( valeurrecuperer.contains("2") && valeurrecuperer.contains("7") && valeurrecuperer.contains("12") && valeurrecuperer.contains("17") ) ||
+	    //Solution 16 : case 7,12,17,22
+	    ( valeurrecuperer.contains("7") && valeurrecuperer.contains("12") && valeurrecuperer.contains("17") && valeurrecuperer.contains("22") ) ||
+	    //Solution 17 : case 3,8,13,18
+	    ( valeurrecuperer.contains("3") && valeurrecuperer.contains("8") && valeurrecuperer.contains("13") && valeurrecuperer.contains("18") ) ||
+	    //Solution 18 : case 8,13,18,23
+	    ( valeurrecuperer.contains("8") && valeurrecuperer.contains("13") && valeurrecuperer.contains("18") && valeurrecuperer.contains("23") ) ||
+	    //Solution 19 : case 4,9,14,19
+	    ( valeurrecuperer.contains("4") && valeurrecuperer.contains("9") && valeurrecuperer.contains("14") && valeurrecuperer.contains("19") ) ||
+	    //Solution 20 : case 9,14,19,24
+	    ( valeurrecuperer.contains("9") && valeurrecuperer.contains("14") && valeurrecuperer.contains("19") && valeurrecuperer.contains("24") ) ||
+	    //Solution 21 : case 0 ,6,12, 18
+	    ( valeurrecuperer.contains("0") && valeurrecuperer.contains("6") && valeurrecuperer.contains("12") && valeurrecuperer.contains("18") ) ||
+	    //Solution 22 : case 6,12,18,24
+	    ( valeurrecuperer.contains("6") && valeurrecuperer.contains("12") && valeurrecuperer.contains("18") && valeurrecuperer.contains("24") )  ||
+	    //Solution 23 : case 4,8,12,16
+	    ( valeurrecuperer.contains("4") && valeurrecuperer.contains("8") && valeurrecuperer.contains("12") && valeurrecuperer.contains("16") ) ||
+	    //Solution 24 : case 8,12,16,20
+	    ( valeurrecuperer.contains("8") && valeurrecuperer.contains("12") && valeurrecuperer.contains("16") && valeurrecuperer.contains("20") )  ||
+	    //Solution 25 : case 1,7,13,19
+	    ( valeurrecuperer.contains("1") && valeurrecuperer.contains("7") && valeurrecuperer.contains("13") && valeurrecuperer.contains("19") ) ||
+	    //Solution 26 : case 5,11,17,23
+	    ( valeurrecuperer.contains("5") && valeurrecuperer.contains("11") && valeurrecuperer.contains("17") && valeurrecuperer.contains("23") ) ||
+	    //Solution 27 : case 3,7,11,15
+	    ( valeurrecuperer.contains("3") && valeurrecuperer.contains("7") && valeurrecuperer.contains("11") && valeurrecuperer.contains("15") ) ||
+	    //Solution 28 : case 5,11,17,23
+	    ( valeurrecuperer.contains("9") && valeurrecuperer.contains("13") && valeurrecuperer.contains("17") && valeurrecuperer.contains("21") )		
+		
+		) {
+			try {
+				new PartieDAO().viderTablePartie();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			req.setAttribute("adressecase"," ");
+			req.getRequestDispatcher("WEB-INF/views/gain.jsp").forward(req, res);
+			//req.setAttribute("gagnant","gagné");
+		}
+		
 		
 		//on parcours la liste valeur recuperer qui est composée d'objet String pour les convertir en int 
 		//et pour la valeur récupérer on assignie X pour l'indice dans le tableau valeurcocheetab
@@ -71,12 +154,20 @@ public class gameServlet extends HttpServlet{
 			valeurcocheetab[toointvaleur]="X";
 		}
 		
+		for (String itemvaleur : valeurrecuperer2) {
+			int toointvaleur = Integer.parseInt(itemvaleur);
+			valeurcocheetab[toointvaleur]="O";
+		}
+		
+		
+		
 		//for (String itemvaleur : valeurcocheetab)
 			//System.out.println(itemvaleur);
 		
 		req.setAttribute("casecochees",valeurcocheetab);
+		
 			
-		req.getRequestDispatcher("WEB-INF/views/game.jsp").include(req, res);
+		req.getRequestDispatcher("WEB-INF/views/jeu.jsp").forward(req, res);
 	}
 
 }
